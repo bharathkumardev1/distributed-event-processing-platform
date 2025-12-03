460b87378333109bb7dd966e00a5c2e3dd19cb52
bharathkumardev1 <iambharathkumar1@gmail.com>
2025-12-03 14:46:48 -0600

    Create EventPayload class for event data management

---
+++ src/main/java/com/bharath/eventplatform/model/EventPayload.java
@@ -0,0 +1,34 @@
+package com.bharath.eventplatform.model;
+
+import java.util.Map;
+
+public class EventPayload {
+
+    private String eventId;
+    private String type;
+    private Map<String, Object> payload;
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Map<String, Object> getPayload() {
+        return payload;
+    }
+
+    public void setPayload(Map<String, Object> payload) {
+        this.payload = payload;
+    }
+}
