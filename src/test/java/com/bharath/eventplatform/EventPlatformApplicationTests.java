a59347cd639f4a6a8d709542513aeaab0e80de8e
bharathkumardev1 <iambharathkumar1@gmail.com>
2025-12-05 09:32:22 -0600

    Add basic context load test

---
+++ src/test/java/com/bharath/eventplatform/EventPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharath.eventplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Verifies that the Spring context starts successfully.
+    }
+}
