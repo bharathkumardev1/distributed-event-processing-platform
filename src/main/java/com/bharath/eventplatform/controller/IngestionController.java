ae6987306f97a18baab30cfc2625361ddceaa34b
bharathkumardev1 <iambharathkumar1@gmail.com>
2025-12-05 09:31:11 -0600

    Add REST ingestion controller

--- src/main/java/com/bharath/eventplatform/controller/IngestionController.java
+++ src/main/java/com/bharath/eventplatform/controller/IngestionController.java
@@ -2,12 +2,16 @@ package com.bharath.eventplatform.controller;
 
 import com.bharath.eventplatform.model.EventPayload;
 import com.bharath.eventplatform.service.EventProducerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/events")
 public class IngestionController {
+
+    private static final Logger log = LoggerFactory.getLogger(IngestionController.class);
 
     private final EventProducerService producerService;
 
@@ -17,6 +21,7 @@ public class IngestionController {
 
     @PostMapping
     public ResponseEntity<Void> ingest(@RequestBody EventPayload payload) {
+        log.info("Received ingestion request: {}", payload);
         producerService.sendEvent(payload);
         return ResponseEntity.accepted().build();
     }
